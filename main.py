9415d450772f5b904deef8a680e2159de91b7917
Manuia Sylvestre <manuia.sylvestre-baron@telecom-sudparis.eu>
2024-06-05 15:20:11 +0200

    test reconstruction

--- main.py
+++ main.py
@@ -30,38 +30,89 @@ def R(M):
         if t==1:
             x+=3
 
-    return A,b
+    r=gauss(A,b)
 
-def reco_R(image,r):
+    return r
+
+def G(M):
+    A=np.zeros((7,7))
+    b=np.zeros(7)
+    x=0
+
+    for i in range(6):
+        t=i%2
+        l = x+1
+        for j in range(6):
+            w = j % 2
+            b[i] = M[l][1-t]
+            terme = M[l][2 +t+(-1)**i*w]
+            A[i][j]=terme
+            if w == 1:
+                l += 1
+        if t==1:
+            x+=3
+    for i in range(7):
+        A[i][6]=M[][0]
+    for j in range(6):
+        A[6][j]=M[][0]
+
+    g=gauss(A,b)
+
+    return g
+
+
+def B(M):
+    A=np.zeros((8,8))
+    b=np.zeros(8)
+    x=0
+
+    for i in range(6):
+        t=i%2
+        l = x+2
+        for j in range(6):
+            w = j % 2
+            b[i] = M[l][1-t]
+            terme = M[l][2 +t+(-1)**i*w]
+            A[i][j]=terme
+            if w == 1:
+                l += 1
+        if t==1:
+            x+=3
+    for i in range(7):
+        A[i][6]=M[][0]
+    for j in range(6):
+        A[6][j]=M[][0]
+
+    for i in range(8):
+        A[i][7]=M[][1]
+    for j in range(8):
+        A[7][j]=M[][1]
+    b=gauss(A,b)
+
+    return b
+
+def reco_R(image,r,g,b):
     img = cv2.imread(image)
-    R_predit=np.zeros(img.shape)
+    Predit=np.zeros(img.shape)
     R,G,B=CanauxDeCouleur(image)
 
     for i in range(1,img.shape[0]):
         for j in range(1,img.shape[1]):
-            R_predit[i][j][0]=int(r[0]*R[i-1][j]+r[1]*R[i][j-1]+r[2]*G[i-1][j]+r[3]*G[i][j-1]+r[4]*B[i-1][j]+r[5]*B[i][j-1])
-            R_predit[i][j][1]=int(r[0]*R[i-1][j]+r[1]*R[i][j-1]+r[2]*G[i-1][j]+r[3]*G[i][j-1]+r[4]*B[i-1][j]+r[5]*B[i][j-1])
-            R_predit[i][j][2]=int(r[0]*R[i-1][j]+r[1]*R[i][j-1]+r[2]*G[i-1][j]+r[3]*G[i][j-1]+r[4]*B[i-1][j]+r[5]*B[i][j-1])
+            Predit[i][j][0]=int(r[0]*R[i-1][j]+r[1]*R[i][j-1]+r[2]*G[i-1][j]+r[3]*G[i][j-1]+r[4]*B[i-1][j]+r[5]*B[i][j-1])
+            Predit[i][j][1]=int(g[0]*R[i-1][j]+g[1]*R[i][j-1]+g[2]*G[i-1][j]+g[3]*G[i][j-1]+g[4]*B[i-1][j]+g[5]*B[i][j-1]+g[6]*Predit[i][j][0])
+            Predit[i][j][2]=int(b[0]*R[i-1][j]+b[1]*R[i][j-1]+b[2]*G[i-1][j]+b[3]*G[i][j-1]+b[4]*B[i-1][j]+b[5]*B[i][j-1]+b[6]*Predit[i][j][0]+b[7]*Predit[i][j][1])
 
-    return R_predit
+
+    return Predit
 
 
 if __name__ == '__main__':
 
-    A,b=R(M)
-    r=gauss(A,b)
-    R_predit=reco_R(image,r)
-    print(R_predit[:,:,0])
-    print(CanauxDeCouleur(image)[0])
 
+    r,g,b=R(M),G(M),B(M)
+    Predit=reco_R(image,r,g,b)
 
     img=cv2.imread(image)
 
-    while True:
-        cv2.imshow('canal-rouge',CanauxDeCouleur(image)[0])
-        cv2.imshow('Predit',R_predit)
-        if cv2.waitKey(0):
-            break
 
-    cv2.destroyAllWindows()
 
